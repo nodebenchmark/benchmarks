a278ddf6a2bb322e0aab533f65afb5a188298a3b
Matthew Halpern <matthalp@utexas.edu>
2015-04-15 20:32:15 -0500

    added more requests to lighter -- be carefule what you wish for!

--- lighter/lighter_nodebench.py
+++ lighter/lighter_nodebench.py
@@ -10,5 +10,14 @@ def get(dummy, url):
     return r.content
 
 user_requests = [
-        partial(get,url=lighter_url)
+        partial(get,url="%s/" % lighter_url),
+        partial(get,url="%s/styles/style.css" % lighter_url),
+        partial(get,url="%s/styles/highlight.css" % lighter_url),
+        partial(get,url="%s/styles/media.css" % lighter_url),
+        partial(get,url="%s/images/line-tile.png" % lighter_url),
+        partial(get,url="%s/images/noise.png" % lighter_url),
+        partial(get,url="%s/images/header-bottom.png" % lighter_url),
+        partial(get,url="%s/styles/fonts/source-sans-pro/SourceSansPro-Regular-webfont.woff" % lighter_url),
+        partial(get,url="%s/styles/fonts/source-sans-pro/SourceSansPro-Black-webfont.woff" % lighter_url),
+        partial(get,url="%s/favicon.ico" % lighter_url),
 ]
