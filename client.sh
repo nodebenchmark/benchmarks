1824a8eed1b8fec3ede291feda73ec3cccde665c
Yuhao Zhu <yzhu@utexas.edu>
2015-05-06 20:28:29 -0500

    updated new versions of harness scripts

--- client.sh
+++ client.sh
@@ -10,38 +10,143 @@ if [ "$mode" = "cache" ]; then
 	size=$4
 	assoc=$5
 	line=$6
+	epsilon=$7
+	policy=$8
+	prefetch=$9
+	missseq=${10}
 
 	if [ "$type" = "inst" ]; then
 		prefix=i
 	else
 		prefix=d
 	fi
-	instrumentation="$PIN_HOME/pin.sh -t $PIN_HOME/source/tools/MyPinTool/obj-intel64/cache.so -${prefix}enable 1 -${prefix}l1size $size -${prefix}l1assoc $assoc -${prefix}l1line $line -o ${prefix}cache.out.${size}.${assoc}.${line} -- "
-	./nodebench -i "$instrumentation" -n $NODE_HOME/node -b $app -c 5
+
+	if [ "$policy" = "opt" ]; then
+		tool=opt_cache
+	elif [ "$policy" = "lru" ]; then
+		tool=cache_lru
+	elif [ "$policy" = "bip" ]; then
+		tool=cache_bip
+	else
+		tool=cache
+	fi
+
+	if [ "$prefetch" = "1" ]; then
+		outfile=${prefix}cache.${policy}.${size}.${assoc}.${line}.prefetch
+		seqoutfile=missseq.prefetch.out
+	else
+		outfile=${prefix}cache.${policy}.${size}.${assoc}.${line}
+		seqoutfile=missseq.out
+	fi
+
+	if [ -s $app/${outfile} ]; then
+		exit
+	fi
+
+	instrumentation="$PIN_HOME/pin.sh -t $PIN_HOME/source/tools/MyPinTool/obj-intel64/${tool}.so -${prefix}enable 1 -${prefix}l1size $size -${prefix}l1assoc $assoc -${prefix}l1line $line -${prefix}l1pf $prefetch -${prefix}l1epsilon $epsilon -${prefix}l1missseq ${missseq} -f ${seqoutfile} -o ${outfile} -- "
+	cd $app
+	eval $(../nodebench -i "$instrumentation" -n $NODE_HOME/node -b $app --server-only) &
+elif [ "$mode" = "intrareuse" ]; then
+	instrumentation="$PIN_HOME/pin.sh -t $PIN_HOME/source/tools/CacheReuse/obj-intel64/intra-reuse.so -- "
+	cd $app
+	eval $(../nodebench -i "$instrumentation" -n $NODE_HOME/node -b $app --server-only) &
+elif [ "$mode" = "interreuse" ]; then
+	instrumentation="$PIN_HOME/pin.sh -t $PIN_HOME/source/tools/CacheReuse/obj-intel64/inter-reuse.so -- "
+	cd $app
+	eval $(../nodebench -i "$instrumentation" -n $NODE_HOME/node -b $app --server-only) &
 elif [ "$mode" = "bp" ]; then
 	scheme=$3
 
 	if [ "$scheme" = "global" ]; then
 		global_size=$4
-		properties="-global-size $global_size -o bp.out.${scheme}.${global_size}"
-	else
+		outfile="bp.out.${scheme}.${global_size}"
+		properties="-global-size $global_size -o ${outfile}"
+	elif [ "$scheme" = "local" ]; then
 		local_size=$4
 		local_histories=$5
-		properties="-local-size $local_size -local-histories $local_histories -o bp.out.${scheme}.${local_size}.${local_histories}"
+		outfile="bp.out.${scheme}.${local_size}.${local_histories}"
+		properties="-local-size $local_size -local-histories $local_histories -o ${outfile}"
+	elif [ "$scheme" = "tournament" ]; then
+		global_size=$4
+		local_size=$5
+		local_histories=$6
+		tournament_size=$7
+		outfile="bp.out.${scheme}.${global_size}.${local_size}.${local_histories}.${tournament_size}"
+		properties="-global-size $global_size -local-size $local_size -local-histories $local_histories -tournament-size $tournament_size -o ${outfile}"
+	fi
+	if [ -s $app/${outfile} ]; then
+		exit
 	fi
 	instrumentation="$PIN_HOME/pin.sh -t $PIN_HOME/source/tools/MyPinTool/obj-intel64/BranchPredictor.so -type $scheme $properties -- "
-	./nodebench -i "$instrumentation" -n $NODE_HOME/node -b $app -c 5
+	cd $app
+	eval $(../nodebench -i "$instrumentation" -n $NODE_HOME/node -b $app --server-only) &
 elif [ "$mode" = "tlb" ]; then
 	sets=$3
 	ways=$4
 	instrumentation="$PIN_HOME/pin.sh -t $PIN_HOME/source/tools/MyPinTool/obj-intel64/tlbs.so -l1-sets $sets -l1-ways $ways -o tlbsim.out.${sets}.${ways} -- "
-	./nodebench -i "$instrumentation" -n $NODE_HOME/node -b $app -c 5
+	if [ -s $app/tlbsim.out.${sets}.${ways} ]; then
+		exit
+	fi
+	cd $app
+	eval $(../nodebench -i "$instrumentation" -n $NODE_HOME/node -b $app --server-only) &
 elif [ "$mode" = "inscount" ]; then
 	instrumentation="$PIN_HOME/pin.sh -t $PIN_HOME/source/tools/MyPinTool/obj-intel64/inscount.so -o inscount.out -- "
-	./nodebench -i "$instrumentation" -n $NODE_HOME/node -b $app -c 5
+	cd $app
+	eval $(../nodebench -i "$instrumentation" -n $NODE_HOME/node -b $app --server-only) &
 elif [ "$mode" = "insmix" ]; then
 	instrumentation="$PIN_HOME/pin.sh -t $PIN_HOME/source/tools/Mica/obj-intel64/mica.so -o insmix.out -- "
-	./nodebench -i "$instrumentation" -n $NODE_HOME/node -b $app -c 5
+	cd $app
+	eval $(../nodebench -i "$instrumentation" -n $NODE_HOME/node -b $app --server-only) &
+elif [ "$mode" = "footprint" ]; then
+	instrumentation="$PIN_HOME/pin.sh -t $PIN_HOME/source/tools/MyPinTool/obj-intel64/InstFootprint.so -o footprint.out -- "
+	cd $app
+	eval $(../nodebench -i "$instrumentation" -n $NODE_HOME/node -b $app --server-only) &
+elif [ "$mode" = "vis" ]; then
+	instrumentation="$PIN_HOME/pin.sh -t $PIN_HOME/source/tools/MyPinTool/obj-intel64/vis.so -o visaddr.out -- "
+	cd $app
+	eval $(../nodebench -i "$instrumentation" -n $NODE_HOME/node -b $app --server-only) &
+elif [ "$mode" = "bt" ]; then
+	instrumentation="$PIN_HOME/pin.sh -t $PIN_HOME/source/tools/MyPinTool/obj-intel64/BranchTargets.so -o bt.out -- "
+	cd $app
+	eval $(../nodebench -i "$instrumentation" -n $NODE_HOME/node -b $app --server-only) &
+elif [ "$mode" = "acs" ]; then
+	instrumentation="$PIN_HOME/pin.sh -t $PIN_HOME/source/tools/MyPinTool/obj-intel64/AcsPattern.so -o bblacs.out -- "
+	cd $app
+	eval $(../nodebench -i "$instrumentation" -n $NODE_HOME/node -b $app --server-only) &
+elif [ "$mode" = "branchcdf" ]; then
+	instrumentation="$PIN_HOME/pin.sh -t $PIN_HOME/source/tools/MyPinTool/obj-intel64/BranchCDF.so -o branchcdf.out -- "
+	cd $app
+	eval $(../nodebench -i "$instrumentation" -n $NODE_HOME/node -b $app --server-only) &
+elif [ "$mode" = "branchalias" ]; then
+	instrumentation="$PIN_HOME/pin.sh -t $PIN_HOME/source/tools/BranchAliasing/obj-intel64/BranchAliasing.so -o branchaliases.out -- "
+	cd $app
+	eval $(../nodebench -i "$instrumentation" -n $NODE_HOME/node -b $app --server-only) &
+elif [ "$mode" = "reuse" ]; then
+	cd $app
+	type=$3
+	if [ "$type" = "event" ]; then
+		cp $PIN_HOME/source/tools/Mica/mica.conf.reuse-event mica.conf
+		outfile="reuse.event.out"
+	else
+		cp $PIN_HOME/source/tools/Mica/mica.conf.reuse-app mica.conf
+		outfile="reuse.app.out"
+	fi
+
+	instrumentation="$PIN_HOME/pin.sh -t $PIN_HOME/source/tools/Mica/obj-intel64/mica.so -o ${outfile} -- "
+	eval $(../nodebench -i "$instrumentation" -n $NODE_HOME/node -b $app --server-only) &
 fi
 
-#/group/users/yzhu/pinplay-drdebug-2.1-pin-2.14-71313-gcc.4.4.7-linux/pin.sh -t /group/users/yzhu/pinplay-drdebug-2.1-pin-2.14-71313-gcc.4.4.7-linux/source/tools/MyPinTool/obj-intel64/icache.so -- /group/users/yzhu/node/node /group/users/yzhu/nodebench/etherpad-lite/node_modules/ep_etherpad-lite/node/server.js &
+rm -f /tmp/$app
+while [ ! -e /tmp/$app ]
+do
+	sleep 2
+done
+sleep 35
+rm /tmp/$app
+../nodebench -b $app -c 100 --client-only
+cd ..
+sleep 5
+
+pid=`ps ux | grep "/group/users/yzhu/benchmarks/node/node" | grep $app | awk '{if(NR==1) print $2}'`
+kill -INT $pid
+sleep 3
--- run.sh
+++ run.sh
@@ -3,43 +3,75 @@ mode=$2
 
 if [ "$mode" = "cache" ]; then
 	type=$3 #inst for icache, and data for dcache
+	policy=$4
+	prefetch=$5
+	missseq=$6
+	epsilon=16
+
 	if [ -z "$type" ]; then
 		echo "which cache type? [inst/data]"
 		exit
 	fi
-	for size in 8 16 32 64 128 256 512 1024
+	#for size in 8 16 32 64 128 256 512 1024
+	for size in 32
 	do
-		for assoc in 4 8
+		#for assoc in 4 8 16
+		for assoc in 8
 		do
-			for line in 16 32 64
+			#for line in 16 32 64 128
+			for line in 64
 			do
-				echo $size $assoc $line
-				sh client.sh $app $mode $type $size $assoc $line
+				echo "["$size $assoc $line"]" "["$policy $epsilon"]" $prefetch
+				sh client.sh $app $mode $type $size $assoc $line $epsilon $policy $prefetch $missseq
 			done
 		done
 	done
+elif [ "$mode" = "intrareuse" ]; then
+	sh client.sh $app $mode
+elif [ "$mode" = "interreuse" ]; then
+	sh client.sh $app $mode
 elif [ "$mode" = "bp" ]; then
 	scheme=$3
 	if [ "$scheme" = "global" ]; then
-		for global_size in 2 4 8 16 32 64 128 256 512 1024 2048 4096
+		#for global_size in 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536
+		for global_size in 128 256 512 1024 2048 4096 65536
 		do
 			echo $global_size
 			sh client.sh $app $mode $scheme $global_size
 		done
 	elif [ "$scheme" = "local" ]; then
-		for local_size in 2 4 8 16 32 64 128 256 512 1024 2048 4096
+		#for local_size in 2 4 8 16 32 64 128 256 512 1024 2048 4096
+		for local_size in 4096
 		do
-			for local_histories in 2 4 8 16 32 64 128 256 512 1024
+			#for local_histories in 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384
+			for local_histories in 16384
 			do
 				echo $local_size $local_histories
 				sh client.sh $app $mode $scheme $local_size $local_histories
 			done
 		done
+	elif [ "$scheme" = "tournament" ]; then
+		global_size=4096
+		local_size=4096
+		local_histories=256
+		tournament_size=4096
+		echo $global_size $local_size $local_histories $tournament_size
+		sh client.sh $app $mode $scheme $global_size $local_size $local_histories $tournament_size
 	else
 		echo "which prediction scheme? [global/local]"
 	fi
 elif [ "$mode" = "tlb" ]; then
-	for sets in 1 2 4 8 16
+	for sets in 2 4 8 16
+	do
+		for ways in 32
+		do
+			echo $sets $ways
+			sh client.sh $app $mode $sets $ways
+		done
+	done
+	exit
+	#for sets in 1 2 4 8 16
+	for sets in 1
 	do
 		for ways in 1 2 4 8 16 32
 		do
@@ -51,6 +83,21 @@ elif [ "$mode" = "inscount" ]; then
 	sh client.sh $app $mode
 elif [ "$mode" = "insmix" ]; then
 	sh client.sh $app $mode
+elif [ "$mode" = "footprint" ]; then
+	sh client.sh $app $mode
+elif [ "$mode" = "vis" ]; then
+	sh client.sh $app $mode
+elif [ "$mode" = "bt" ]; then
+	sh client.sh $app $mode
+elif [ "$mode" = "acs" ]; then
+	sh client.sh $app $mode
+elif [ "$mode" = "branchcdf" ]; then
+	sh client.sh $app $mode
+elif [ "$mode" = "branchalias" ]; then
+	sh client.sh $app $mode
+elif [ "$mode" = "reuse" ]; then
+	type=$3 #event for per-event, and app for the whole application
+	sh client.sh $app $mode $type
 else
 	echo "which mode? [cache/bp/tlb]"
 fi
