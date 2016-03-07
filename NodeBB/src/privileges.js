9713a2a37cf6c127dfc6761750abf8eead8dc36b
“Yuhao <queeten@gmail.com>
2016-03-06 23:43:32 -0600

    Remove NodeBB and Nodejs-chat.

--- NodeBB/src/privileges.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var privileges = {};
-
-require('./privileges/categories')(privileges);
-require('./privileges/topics')(privileges);
-require('./privileges/posts')(privileges);
-
-module.exports = privileges;
